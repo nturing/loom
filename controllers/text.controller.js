f77c4731cc3d1b04c0c49d7eafc46325a0c846af
Shreya Belagavi <shreyabelagavi3111@gmail.com>
2024-07-27 17:45:51 +0530

    text controller: applied a patch for fixing jsdoc documentation pattern

--- controllers/text.controller.js
+++ controllers/text.controller.js
@@ -1,23 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.handlePlainText = (req, res) => {
-  const text = req.body;
-  console.log('Plain Text:', text);
-  res.send('Plain text received');
-};
-
 /**
  * Handles plain text input by logging it and sending a response.
  * @param {Object} req - The request object, containing the plain text in `req.body`.
  * @param {Object} res - The response object used to send a response back to the client.
  * @returns {void}
  */
-
-exports.handleJson = (req, res) => {
-  const jsonData = req.body;
-  console.log('JSON Data:', jsonData);
-  res.send('JSON data received');
+exports.handlePlainText = (req, res) => {
+  const text = req.body;
+  console.log('Plain Text:', text);
+  res.send('Plain text received');
 };
 
 /**
@@ -26,7 +19,18 @@ exports.handleJson = (req, res) => {
  * @param {Object} res - The response object used to send a response back to the client.
  * @returns {void}
  */
+exports.handleJson = (req, res) => {
+  const jsonData = req.body;
+  console.log('JSON Data:', jsonData);
+  res.send('JSON data received');
+};
 
+/**
+ * Handles text file uploads by reading the file content and sending a response.
+ * @param {Object} req - The request object, containing the file information in `req.file`.
+ * @param {Object} res - The response object used to send a response back to the client.
+ * @returns {void}
+ */
 exports.handleTextFile = (req, res) => {
   const filePath = req.file.path;
   fs.readFile(filePath, 'utf8', (err, data) => {
@@ -36,11 +40,4 @@ exports.handleTextFile = (req, res) => {
     console.log('Text File Content:', data);
     res.send('Text file received');
   });
-
-  /**
- * Handles text file uploads by reading the file content and sending a response.
- * @param {Object} req - The request object, containing the file information in `req.file`.
- * @param {Object} res - The response object used to send a response back to the client.
- * @returns {void}
-   */
 };
